88a089317581a7e8c49ec57881a7e7c5eeb1097c
Victoria <victoria.ad40@gmail.com>
2022-12-14 17:56:53 -0300

    first commit

---
+++ src/controllers/PokemonController.js
@@ -0,0 +1,1 @@
+const PokemonService = require('../services/PokemonService')
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+const express = require('express')
+
+const router = express.Router()
+const PokemonControler = require('./controllers/PokemonController')
+module.exports = router
